c4c480e6eee400286779b6a51fc4bdfb88028eb9
Angelica Grass <ag223vg@student.lnu.se>
2021-03-28 09:40:19 +0200

    Latest changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className='py-3'>
         <Container>
           <h1>Här är mitt projekt</h1>
         </Container>
