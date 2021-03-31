81d5f12fe13a1922da8c824cf9e57ba357ab7dec
Angelica Grass <ag223vg@student.lnu.se>
2021-03-31 08:53:48 +0200

    product.js

---
+++ src/products.js
@@ -0,0 +1,43 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Poster 1',
+    image: '/img/produkt1.jpg',
+    description:
+      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 79,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Familjeposter 2',
+    image: '/img/produkt2.jpg',
+    description:
+      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Poster3',
+    image: '/img/produkt3.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 59,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+]
+
+export default products
