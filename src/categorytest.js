0df813cd83d6cefc5ff9b7d92f435ba7eb91f1a1
Angelica Grass <ag223vg@student.lnu.se>
2021-03-31 08:59:55 +0200

    Package.json and test products

---
+++ src/categorytest.js
@@ -0,0 +1,29 @@
+const categories = [
+  {
+    _id: '1',
+    name: 'Posters',
+  },
+  {
+    _id: '2',
+    name: 'Fotokonst',
+  },
+  {
+    _id: '3',
+    name: 'Personliga Posters',
+  },
+  {
+    _id: '4',
+    name: 'Barntavlor',
+  },
+  {
+    _id: '5',
+    name: 'Bröllopsposter',
+  },
+  {
+    _id: '6',
+    name: 'Doptavlor',
+  },
+  
+]
+
+export default categories
