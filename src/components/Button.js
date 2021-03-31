3922f6bcdfaadc909972c7f29a31ba9fc723466b
Angelica Grass <ag223vg@student.lnu.se>
2021-03-31 08:57:29 +0200

    Button component

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const StyledButton = styled.button``
+
+const Button = () => {
+  return (
+    <div>
+   <StyledButton></StyledButton>
+      
+    </div>
+  )
+}
+
+export default Button
