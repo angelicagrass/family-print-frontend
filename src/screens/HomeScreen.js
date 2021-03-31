e74398bb05a9abfb56fad022332c520c97bfb79b
Angelica Grass <ag223vg@student.lnu.se>
2021-03-31 08:56:34 +0200

    Homescreen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainBox from '../components/Main.js'
+import LatestProducts from '../components/LatestProducts'
+
+const HomeScreen = () => {
+  return (
+      <MainBox>
+        <LatestProducts />
+        <h1>Homescreenroute</h1>
+      </MainBox>
+  )
+}
+
+export default HomeScreen
