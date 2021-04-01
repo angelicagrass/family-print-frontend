df19bab80aa5e5029985a9692921f7be620c96e2
Angelica Grass <ag223vg@student.lnu.se>
2021-04-01 14:09:42 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 // import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Footer from './components/Footer.js'
 import Navigation from './components/Navbar.js'
 import Categories from './components/Categories.js'
 import HomeScreen from './screens/HomeScreen.js'
+import ProductScreen from './screens/ProductScreen.js'
+
 
 const App = () => {
   return (
-    
     <Router>
-    <Navigation />
+       <Categories />
+        <Navigation />
       {/* <HeaderImage /> */}
-      <Categories />
         <Route path='/' component={HomeScreen} exact />
+        <Route path='/product/:id/' component={ProductScreen} exact />
       <Footer />
     </Router>
   );
