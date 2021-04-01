ad777b493501435f7266fee64ecce8c48e209f42
Angelica Grass <ag223vg@student.lnu.se>
2021-04-01 14:07:23 +0200

    Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-const StyledButton = styled.button``
+const StyledButton = styled.button`
+padding:0.3em 1.2em;
+font-size: 1rem;
+background-color: ${props => props.theme.colors.main};
+outline: none;
+border: none;
+color: white;
+border-radius: 2em;
+transition: all 0.2s;
 
-const Button = () => {
-  return (
-    <div>
-   <StyledButton></StyledButton>
-      
-    </div>
-  )
+&:hover {
+  background-color: ${props => props.theme.colors.lightmain};
+}
+`
+
+const Button = ({ primary, children }) => {
+  return <StyledButton primary={primary}>{ children }</StyledButton>
 }
 
 export default Button
