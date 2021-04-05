35de58915c2e2a2401ee1582672b378713ef28a0
Angelica Grass <ag223vg@student.lnu.se>
2021-04-05 10:48:50 +0200

    Latestproducts

--- src/components/LatestProducts.js
+++ src/components/LatestProducts.js
@@ -2,11 +2,19 @@ import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import products from '../products.js'
 import Product from './Product.js'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  margin-top: 5vh;
+
+
+`
+
 
 const LatestProducts = () => {
   return (
-    <>
-    <h1>Senaste produkterna</h1>
+    <StyledDiv>
+    <h1 class="text-muted">Nyheter</h1>
     <Row className="justify-content-md-center">
       {products.map(product => (
         <Col sm={12} md={6} lg={4} xl={3}>
@@ -14,7 +22,7 @@ const LatestProducts = () => {
         </Col>
       ))} 
     </Row>  
-    </>
+    </StyledDiv>
   )
 }
 
