fe54995449ac2dfd37047ae1fa716791a5ed4f2b
Angelica Grass <ag223vg@student.lnu.se>
2021-04-05 10:48:41 +0200

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,16 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import Links from './Links.js'
-import styled from 'styled-components'
 
-
-// const StyleNav = styled.header`
-// min-height: 30vh;
-
-// `
 
 const Navigation = () => {
     return (
         <header>
             <Navbar className="navbar" bg="light" variant='light' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="#home"></Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
-                         <Nav className="ml-auto">
+                         <Nav className="mr-auto">
                            <Links />
                         </Nav>
                     </Navbar.Collapse>
