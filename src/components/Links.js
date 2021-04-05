2f67906787d209684fa510899f19a94623a569d0
Angelica Grass <ag223vg@student.lnu.se>
2021-04-05 10:48:24 +0200

    Links

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, NavDropdown } from 'react-bootstrap'
 import categories from '../categorytest.js'
 import slugify from 'slugify'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-
 
 const StyledLink = styled(Link)`
     text-decoration: none;
@@ -16,7 +15,6 @@ const StyledLink = styled(Link)`
   &:hover {
     color: grey;
   }
-
 `
 
 const Links = () => {
@@ -25,10 +23,16 @@ const Links = () => {
       <Row>
       {categories.map(category => (
         <Col>
-          <StyledLink to={'/' + slugify(category.name, {
-            lower: true,
-           }
-          )}>{category.name}</StyledLink>
+          <NavDropdown title={category.name} id="basic-nav-dropdown">
+            {category.sub.map((sub) => {
+              return (
+              <NavDropdown.Item>
+                <StyledLink to={'/' + slugify(category.name, {lower: true}) + '/' + slugify(sub, {lower: true})
+                  }>{sub}</StyledLink>
+              </NavDropdown.Item>
+              )
+            })}
+          </NavDropdown>
         </Col>
       ))} 
       </Row>
