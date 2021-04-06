760f807117111545ce2052f2cc59489a5b9d6498
Angelica Grass <ag223vg@student.lnu.se>
2021-04-06 10:23:41 +0200

    Find products in category on click

---
+++ src/screens/CategoryScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import MainBox from '../components/Main.js'
+import products from '../products.js'
+import { useParams } from 'react-router-dom'
+import Product from '../components/Product.js'
+import { Col, Row } from 'react-bootstrap'
+
+
+const CategoryScreen = () => {
+  const { category, subcategory } = useParams()
+
+  const product = products.filter((p) => p.subcategory === subcategory)
+  console.log(product)
+  
+  return (
+    <MainBox>
+     <Row className="justify-content-md-center">
+        {product.map(product => (
+          <Col sm={12} md={6} lg={4} xl={3}>
+          <Product product={product} />
+          </Col>
+        ))} 
+      </Row>
+    </MainBox>
+  )
+}
+
+export default CategoryScreen
