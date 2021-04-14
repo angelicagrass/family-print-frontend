b5b3ef80c3aeae6742c4596acbf0360d16dc1feb
Angelica Grass <ag223vg@student.lnu.se>
2021-04-14 09:27:16 +0200

    Shadow on products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import styled from 'styled-components'
+
+const MyCard = styled(Card)`
+transition: all 0.6s;
+
+&:hover {
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 20px 30px;
+}
+`
 
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <MyCard className='my-3 p-3 rounded'>
       <a href={`/product/${product.id}`}>
         <Card.Img src={product.imageurl} variant='top' />
       </a>
@@ -14,7 +23,7 @@ const Product = ({ product }) => {
       </a>
       <Card.Text as='p'>{product.price} sek </Card.Text>
       </Card.Body>
-    </Card>
+    </MyCard>
   )
 }
 
