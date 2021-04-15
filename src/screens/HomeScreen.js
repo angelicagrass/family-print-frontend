a50048261894da05b0406c489ddfb48585e66d13
Angelica Grass <ag223vg@student.lnu.se>
2021-04-15 09:02:02 +0200

    Themes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,12 +2,14 @@ import React from 'react'
 import MainBox from '../components/Main.js'
 import LatestProducts from '../components/LatestProducts'
 import StartImages from '../components/StartImages.js'
+import CustomCard from '../components/CustomCard.js'
 
 const HomeScreen = () => {
   return (
       <MainBox>
         <StartImages />
         <LatestProducts />
+        <CustomCard></CustomCard>
       </MainBox>
   )
 }
