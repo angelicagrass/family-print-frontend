8fa7352ba0af84a47a25076c9f27df5570fe84f5
Angelica Grass <ag223vg@student.lnu.se>
2021-04-19 10:29:00 +0200

    Cleaning up the code

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,16 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 const CatBar = styled.div`
-background-color: white;
-height: 60px;
-width: 100%;
-padding: 17px;
+  background-color: white;
+  height: 60px;
+  width: 100%;
+  padding: 17px;
 `
 
 const Categories = () => {
     return (
-        <CatBar>
-        </CatBar>
+        <CatBar />
     )
 }
 
