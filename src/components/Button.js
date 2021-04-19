6424d8d1083fc7edcbf131ed6636593296c2755e
Angelica Grass <ag223vg@student.lnu.se>
2021-04-19 10:29:00 +0200

    Cleaning up the code

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,15 +2,15 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledButton = styled.button`
-padding:0.6em 1.3em;
-width: 100%;
-font-size: 1rem;
-background-color: ${props => props.theme.colors.main};
-outline: none;
-border: none;
-color: white;
-border-radius: 1px;
-transition: all 0.4s;
+  padding:0.6em 1.3em;
+  width: 100%;
+  font-size: 1rem;
+  background-color: ${props => props.theme.colors.main};
+  outline: none;
+  border: none;
+  color: white;
+  border-radius: 1px;
+  transition: all 0.4s;
 
 &:hover {
   background-color: ${props => props.theme.colors.lightmain};
