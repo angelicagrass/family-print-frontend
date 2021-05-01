5c806d77ed859bcbe78a08d46097ea7bde66aabd
Angelica Grass <ag223vg@student.lnu.se>
2021-05-01 07:13:09 +0200

    Minor fixes

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -27,7 +27,6 @@ const IcondDiv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
 `
 
 const StyledSelect = styled.select`
@@ -69,8 +68,6 @@ const CustomOptions = styled.option`
     -webkit-box-shadow:0 1px 2px rgba(0, 0, 0, 0.2);
     -moz-box-shadow:0 1px 2px rgba(0, 0, 0, 0.2);
     box-shadow:0 1px 2px rgba(0, 0, 0, 0.2);
-
-
 `
 
 const SelectList = () => {
