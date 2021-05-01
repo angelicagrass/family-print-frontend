44b06b92d6c44c2c0ededcfc76c322854d09b1c1
Angelica Grass <ag223vg@student.lnu.se>
2021-05-01 07:23:49 +0200

    Set upstream

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -75,9 +75,9 @@ const SelectList = () => {
     <StyledDiv>
       <StyledSelect class="form-select" aria-label="Default select example">
             <CustomOptions selected>STORLEK</CustomOptions>
-            <option value="1">A4</option>
-            <option value="2">30 x 40cm</option>
-            <option value="3">50 x 70cm</option>
+              <option value="1">A4</option>
+              <option value="2">30 x 40cm</option>
+              <option value="3">50 x 70cm</option>
       </StyledSelect>
       <IcondDiv><i class="fas fa-chevron-circle-down fa-lg"></i></IcondDiv>
     </StyledDiv>
