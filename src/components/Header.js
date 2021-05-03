73acfb84df8d91fb870c2abfdb4e1fd4b8d5a3e4
Angelica Grass <ag223vg@student.lnu.se>
2021-05-03 13:20:22 +0200

    Header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,41 +3,38 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const GradientDiv = styled.div`
-    width: auto;
-    height: 1vh;
-    background: rgb(168,219,168);
-    background: linear-gradient(37deg, rgba(168,219,168,1) 17%, rgba(131,175,155,1) 49%, rgba(100,159,131,1) 95%);
+  width: auto;
+  height: 1vh;
+  background: rgb(168,219,168);
+  background: linear-gradient(37deg, rgba(168,219,168,1) 17%, rgba(131,175,155,1) 49%, rgba(100,159,131,1) 95%);
 `
 
 const StyledHeader = styled.div`
-    background-color: white;
-    width: 100%auto;
-    border-bottom: 2px solid ${props => props.theme.colors.main};
+  background-color: white;
+  width: 100%auto;
+  border-bottom: 2px solid ${props => props.theme.colors.main};
 `
 
 const StyledImg = styled.div`
-    background-image: url('/img/svart2.png');
-    height: 210px;
-    background-repeat: no-repeat;
-    background-position: center;
+  background-image: url('/img/svart2.png');
+  height: 210px;
+  background-repeat: no-repeat;
+  background-position: center;
 `
 
 const StyledBag = styled.div`
-    float:right;
-    margin: 3vh;
-    color: ${props => props.theme.colors.main};
+  float:right;
+  margin: 3vh;
+  color: ${props => props.theme.colors.main};
 `
 
 const Header = () => {
     return (
         <StyledHeader>
             <StyledBag>
-                <Link to='/kassa'>
-                    <i class="fas fa-shopping-bag fa-3x"></i>
-                </Link>
-                
-                </StyledBag>
-                <StyledImg />
+                <Link to='/kassa'> <i class="fas fa-shopping-bag fa-3x"></i> </Link>
+            </StyledBag>
+            <StyledImg />
             <GradientDiv></GradientDiv>
         </StyledHeader>
     )
