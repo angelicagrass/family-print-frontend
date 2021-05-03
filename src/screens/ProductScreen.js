c95bf43115a61172d388d62c7b759d947ace7a0f
Angelica Grass <ag223vg@student.lnu.se>
2021-05-03 13:17:04 +0200

    Map in productscreen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -8,19 +8,46 @@ import { StateContext } from '../globalstate/GlobalState.js'
 import SelectedList from '../components/SelectList.js'
 
 const MyDiv = styled.div`
-float: left;
-margin-top: -4vh;
+  float: left;
+  margin-top: -4vh;
 `
 
 const ThumbImagesDiv = styled.div`
+  width: 100%;
+  height: 100px;
+  display: flex;
+  cursor: pointer;
+  margin: 10px;
+  justify-content: center;
+`
 
-
+const MyImage = styled.img`
+  width: 90px;
+  height: 100%;
+  display: block;
+  object-fit: cover;
+  border: 1px solid #ddd;
+  margin: 5px 5px;
+  opacity: 0.7;
+  border-radius: 5px;
+  
+  &:active {
+    opacity: 1;
+    border: 3px solid ${props => props.theme.colors.main};
+  }
+  &:hover {
+    opacity: 1;
+  }
+`
+const InfoDiv = styled.div`
+  padding-top: 40px;
 `
 
 const ProductScreen = () => {
   const { id } = useParams()
   const [product, setProduct] = useState([])
   const { cartItems, setCartItems } = React.useContext(StateContext)
+  const [arrayIndex, setArrayIndex] = useState(0)
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -33,42 +60,40 @@ const ProductScreen = () => {
 
   return (
   <MainBox>
-    <MyDiv>
+    {/* <MyDiv>
       <Link to='/'>
         <Button>Gå tillbaka</Button>
       </Link>
-    </MyDiv>
-
-    <Row className='justify-content-center'>
-      <Col md={5}>
-        <Image src={product.imageurl} alt={product.name} fluid />
-      </Col>
-      <Col md={4}>
-        <ListGroup variant='flush'>
+    </MyDiv> */}
+      <Row className='justify-content-center'>
+        <Col md={5}>
+          <Image src={product.thumbImageUrls && product.thumbImageUrls[arrayIndex]} alt={product.name} fluid /> </Col>
+        <Col md={4}>
+          <ListGroup variant='flush'>
             <ListGroup.Item>
-              <h2>{product.name}</h2>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              Pris: {product.price} sek
-            </ListGroup.Item>
+              <h2>{product.name}</h2> </ListGroup.Item>
+            <ListGroup.Item> Pris: {product.price} sek </ListGroup.Item>
             <ListGroup.Item>
               <SelectedList />
-                <Button onClick={() => {
-                setCartItems([...cartItems, {...product}])
-                console.log(cartItems)}}>KÖP</Button>
+              <Button onClick={()=> { setCartItems([...cartItems, {...product}]) 
+              }}>KÖP</Button>
             </ListGroup.Item>
-            <ListGroup.Item>
-              Beskrivning: {product.caption}
-            </ListGroup.Item> 
-        </ListGroup>
-      </Col>
-    </Row>
-    <ThumbImagesDiv></ThumbImagesDiv>
-
-      <br></br>
-      <p><i className="fas fa-check-circle fa-lg"></i> Betala säkert med Klarna Checkout!</p>
-      <p><i className="fas fa-check-circle fa-lg"></i> Frågor? Maila oss på info@familyprint.se</p>
-      <p><i className="fas fa-check-circle fa-lg"></i> Leveranstid upp till 10 arbetsdagar.</p>
+            <ListGroup.Item> Beskrivning: {product.caption} </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    <ThumbImagesDiv>
+        {product.thumbImageUrls &&
+          product.thumbImageUrls.map((img, index) => { return (
+            <MyImage src={img} alt="" key={index}
+            onClick={() => setArrayIndex(index)}/>
+          )})} 
+    </ThumbImagesDiv>
+      <InfoDiv>
+        <p><i className="fas fa-check-circle fa-lg"></i> Betala säkert med Klarna Checkout!</p>
+        <p><i className="fas fa-check-circle fa-lg"></i> Frågor? Maila oss på info@familyprint.se</p>
+        <p><i className="fas fa-check-circle fa-lg"></i> Leveranstid upp till 10 arbetsdagar.</p>
+      </InfoDiv>
   </MainBox>
   )
 }
