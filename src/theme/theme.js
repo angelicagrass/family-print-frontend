019ea58146d5a51aec94eeb2895db69c92699d87
Angelica Grass <ag223vg@student.lnu.se>
2021-05-03 13:16:48 +0200

    gradient in theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -4,6 +4,7 @@ const Theme = {
     lightmain: '#A8DBA8',
     dark: '#A6ADA7',
     light: '#AEBFAF', 
+    gradient: 'linear-gradient(37deg, rgba(168,219,168,1) 17%, rgba(131,175,155,1) 49%, rgba(100,159,131,1) 95%)',
   },
   screen: {
     mobile: '650px',
