997c7218bffebc20e9feee2319bd8baadc5b7f98
Angelica Grass <ag223vg@student.lnu.se>
2021-05-05 09:54:52 +0200

    Adding qty to globalstate

--- src/globalstate/GlobalState.js
+++ src/globalstate/GlobalState.js
@@ -3,20 +3,29 @@ import React, { useEffect, useState } from 'react'
 export const StateContext = React.createContext(null)
 
 export default function GlobalState({ children }) {
+  const initialQty = Number(localStorage.getItem('qtyInCart') || 0)
   
   const itemsInCartFromLocalStorage = localStorage.getItem('itemsInCart') ? JSON.parse(localStorage.getItem('itemsInCart')) : []
   const [cartItems, setCartItems] = useState([itemsInCartFromLocalStorage])
+  const [qty, setQty] = useState(initialQty)
   
  
   const state = {
     setCartItems: setCartItems,
-    cartItems: cartItems
+    cartItems: cartItems,
+    setQty: setQty,
+    qty: qty
   }
 
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems))
 
   }, [cartItems])
+
+  useEffect(() => {
+    localStorage.setItem("qtyInCart", qty)
+
+  }, [qty])
 
   return (
     <StateContext.Provider value={state}>
