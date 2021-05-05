ae3365e18a6a825076dda5b3d1fcd8b0fb25649a
Angelica Grass <ag223vg@student.lnu.se>
2021-05-05 10:01:54 +0200

    removed files

--- src/globalstate/GlobalState.js
+++ src/globalstate/GlobalState.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 export const StateContext = React.createContext(null)
 
 export default function GlobalState({ children }) {
-  const initialQty = Number(localStorage.getItem('qtyInCart') || 0)
+  const initialQty = () => Number(localStorage.getItem('qtyInCart') || 0)
   
   const itemsInCartFromLocalStorage = localStorage.getItem('itemsInCart') ? JSON.parse(localStorage.getItem('itemsInCart')) : []
   const [cartItems, setCartItems] = useState([itemsInCartFromLocalStorage])
