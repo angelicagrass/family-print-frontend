3f9c56c2747a887d759857591eaf35a534242be0
Angelica Grass <ag223vg@student.lnu.se>
2021-05-06 14:54:30 +0200

    removed useEffect

--- src/globalstate/GlobalState.js
+++ src/globalstate/GlobalState.js
@@ -4,28 +4,25 @@ export const StateContext = React.createContext(null)
 
 export default function GlobalState({ children }) {
   const initialQty = () => Number(localStorage.getItem('qtyInCart') || 0)
+  // const initialItems = () => JSON.parse(localStorage.getItem('cartItems')) || []
 
-  
-  const itemsInCartFromLocalStorage = () => localStorage.getItem('itemsInCart') ? JSON.parse(localStorage.getItem('itemsInCart')) : []
-  const [cartItems, setCartItems] = useState([itemsInCartFromLocalStorage])
-  const [qty, setQty] = useState(initialQty)
-  
+  const itemsInCartFromLocalStorage = () => localStorage.getItem('cartItems') 
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+  const [cartItems, setCartItems] = useState(itemsInCartFromLocalStorage())
+ 
   const state = {
     setCartItems: setCartItems,
     cartItems: cartItems,
-    setQty: setQty,
-    qty: qty
   }
+
 
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems))
-
   }, [cartItems])
 
-  useEffect(() => {
-    localStorage.setItem("qtyInCart", qty)
 
-  }, [qty])
 
   return (
     <StateContext.Provider value={state}>
