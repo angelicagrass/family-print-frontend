08bdc54d50f46bb326b0371c09c6ace798204db6
Angelica Grass <ag223vg@student.lnu.se>
2021-05-10 20:58:00 +0200

    Parallax

---
+++ src/components/Parallax/Parallax.js
@@ -0,0 +1,28 @@
+import { Parallax, Background } from 'react-parallax';
+import React from 'react'
+import { MyWrapper, MySmallWrapper, MySmallDiv, MyDiv, MyLink } from './StyledParallax.js'
+
+const MyParallax = ({ children }) => {
+    return (
+    <>
+        <MyWrapper>
+            <Parallax blur={{ min: -15, max: 50 }} strength={600}>
+                <Background href='/' className="custom-bg"> <img src="/img/paratest2.png" alt="fill murray" /> </Background>
+                <MyDiv>
+                    <MyLink to='/kassa'> <i className="fas fa-shopping-bag fa-3x"></i> </MyLink>
+                </MyDiv>
+            </Parallax>
+        </MyWrapper>
+        <MySmallWrapper>
+            <Parallax blur={{ min: -15, max: 50 }} strength={800}>
+                <Background href='/' className="custom-bg"> <img src="/img/mobilelogoXS.png" alt="fill murray" /> </Background>
+                <MySmallDiv>
+                    <MyLink to='/kassa'> <i className="fas fa-shopping-bag fa-3x"></i> </MyLink>
+                </MySmallDiv>
+            </Parallax>
+        </MySmallWrapper>
+    </>
+    )
+}
+
+export default MyParallax
