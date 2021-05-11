3f138d7d9255f617d73238e5d58bbb4dbbb308c5
Angelica Grass <ag223vg@student.lnu.se>
2021-05-11 21:10:46 +0200

    Animation in globalstate

--- src/globalstate/GlobalState.js
+++ src/globalstate/GlobalState.js
@@ -3,26 +3,28 @@ import React, { useEffect, useState } from 'react'
 export const StateContext = React.createContext(null)
 
 export default function GlobalState({ children }) {
-  const initialQty = () => Number(localStorage.getItem('qtyInCart') || 0)
-  // const initialItems = () => JSON.parse(localStorage.getItem('cartItems')) || []
 
   const itemsInCartFromLocalStorage = () => localStorage.getItem('cartItems') 
   ? JSON.parse(localStorage.getItem('cartItems'))
   : []
 
   const [cartItems, setCartItems] = useState(itemsInCartFromLocalStorage())
+  const [animation, setAnimation] = useState(false)
  
   const state = {
     setCartItems: setCartItems,
     cartItems: cartItems,
+    animation: animation,
+    setAnimation: setAnimation
   }
-
 
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems))
   }, [cartItems])
 
-
+  useEffect(() => {
+    console.log('ANIMERA')
+  }, [animation])
 
   return (
     <StateContext.Provider value={state}>
