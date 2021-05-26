0bb2374270555fac89908fb1fd184298aee57ecc
Angelica Grass <ag223vg@student.lnu.se>
2021-05-26 20:49:28 +0200

    Styled cartcounter

---
+++ src/components/CartCounter/StyledCartCounter.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const MyDiv = styled.div`
+position: relative;
+float: right;
+background: ${props => props.theme.colors.lightmain};
+border-radius: 50%;
+padding: 5px 5px;
+height: 30px;
+width: 30px;
+text-align: center;
+color: black;
+font-weight: 900;
+margin-right: 60px;
+margin-left: -90px;
+margin-top: 30px;
+opacity: 0.8;
+
+`
