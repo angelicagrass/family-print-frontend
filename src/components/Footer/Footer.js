c1500c9f0f522d474df3ac6198e16e2ada6710b7
angelicagrass <70286909+angelicagrass@users.noreply.github.com>
2021-05-27 19:14:43 +0200

    Link in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact"
 import { StyledBottom, SocialDiv, KlarnaDiv } from './StyledFooter.js'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
@@ -9,8 +10,8 @@ const Footer = () => {
         <MDBRow>
           <MDBCol md="6">
             <h5 className="title">Family Print</h5>
-            <p><a href="/kopvillkor">Köpvillkor &amp; Info</a></p>
-            <SocialDiv> <i className="fab fa-facebook fa-2x darkcolor"></i> <i className="fab fa-instagram-square fa-2x darkcolor"></i> <i className="fas fa-envelope fa-2x darkcolor"></i> </SocialDiv>
+            <p><Link to="/kopvillkor">Köpvillkor &amp; Info</Link></p>
+            <SocialDiv> <a href="https://www.facebook.com"><i className="fab fa-facebook fa-2x darkcolor"></i></a> <i className="fab fa-instagram-square fa-2x darkcolor"></i> <i className="fas fa-envelope fa-2x darkcolor"></i> </SocialDiv>
           </MDBCol>
           <MDBCol md="6"></MDBCol>
         </MDBRow>
