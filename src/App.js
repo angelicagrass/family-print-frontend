2ee88b4d7061f7d8337e738cb1a61a96b7b5f364
Angelica Grass <ag223vg@student.lnu.se>
2021-05-27 09:05:24 +0200

    App

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import GlobalState from './globalstate/GlobalState.js'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Footer from './components/Footer/Footer.js'
 import Navigation from './components/Navbar/Navbar.js'
-import HomeScreen from './screens/HomeScreen.js'
-import ProductScreen from './screens/ProductScreen.js'
-import TermsScreen from './screens/TermsScreen.js'
-import CategoryScreen from './screens/CategoryScreen.js'
+import HomeScreen from './screens/HomeScreen/HomeScreen.js'
+import ProductScreen from './screens/ProductScreen/ProductScreen.js'
+import TermsScreen from './screens/TermsScreen/TermsScreen.js'
+import CategoryScreen from './screens/CategoryScreens/CategoryScreen.js'
 import MyParallax from './components/Parallax/Parallax.js'
-import CartScreen from './screens/CartScreen.js'
+import CartScreen from './screens/CartScreen/CartScreen.js'
 
 
 
