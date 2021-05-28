490ca1586fdfd515031cc301b093dcf85020092e
angelicagrass <70286909+angelicagrass@users.noreply.github.com>
2021-05-28 11:31:30 +0200

    Glassmorphism on discount

--- src/components/DiscountBox/StyledDiscountBox.js
+++ src/components/DiscountBox/StyledDiscountBox.js
@@ -11,16 +11,23 @@ height: 100%;
 export const DiscountDiv = styled.div`
 position: absolute;
 z-index: 100000000;
-border: 2px solid ${props => props.theme.colors.main};
+border: 3px solid transparent;
 width: 400px;
 height: 250px;
 margin-left: auto;
 margin-right: auto;
-background-color: beige;
+background-color: rgba(255,255,255,0.1);
+backdrop-filter: blur(10px);
+background-clip: padding-box;
+box-shadow: rgba(0, 0, 0, 0.2) 0px 20px 30px;
 border-radius: 4px;
 padding: 30px 20px;
 top: -400px;
 animation: drop 1s ease forwards;
+
+@-moz-document url-prefix() {
+  background-color: rgba(255,255,255,0.1);
+}
 
 @keyframes drop {
 0%{}
