7235b019c01041702895b713827e0e723dadfadb
angelicagrass <70286909+angelicagrass@users.noreply.github.com>
2021-06-02 14:41:25 +0200

    Change imports in cartscreen

--- src/screens/CartScreen/CartScreen.js
+++ src/screens/CartScreen/CartScreen.js
@@ -1,28 +1,21 @@
 
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import MainBox from '../../components/Main/Main.js'
 import { StateContext } from '../../globalstate/GlobalState.js'
-import { Col, Image, Row } from 'react-bootstrap'
+import { Col, Image } from 'react-bootstrap'
 import Button from '../../components/Button/Button.js'
 import Dot from '../../importedComponents/Dot.js'
 import DiscountBox from '../../components/DiscountBox/DiscountBox.js'
-import { MyContainer, MyRow, OrderDiv, MyText, HeaderLine, MyImage, PriceDiv, DiscountDiv, RedText, StyledNumber, StyledCounter, TotalDiv, TotalRow } from './StyledCartScreen.js'
-
-
-
+import { MyContainer, MyRow, OrderDiv, MyText, HeaderLine, MyImage, PriceDiv, RedText, StyledNumber, StyledCounter, TotalDiv, KlarnaDiv } from './StyledCartScreen.js'
 
 const CartScreen = () => {
-  const { cartItems, setCartItems } = React.useContext(StateContext)
-  const { animation, setAnimation } = React.useContext(StateContext)
-  const { discountValue } = React.useContext(StateContext)
-  const { counter, setCounter } = React.useContext(StateContext)
-  const [shipping, setShipping] = useState(49)
+  const { cartItems, setCartItems, setAnimation, discountValue, counter, setCounter } = React.useContext(StateContext)
 
-  
   const totalPrice = [...cartItems].reduce((total, obj) => obj.price * obj.qtyInCart + total,0)
   const withDiscount = Math.round(totalPrice * discountValue)
   const theDiscountValue = totalPrice - withDiscount
+  const myShipping = totalPrice > 499 ? 0 : 49
 
   function decrementQty (index) {
     const newArray = [...cartItems]
@@ -47,66 +40,55 @@ const CartScreen = () => {
     setCartItems(newArray.filter(item => item !== newArray[index]))
   }
 
-
-
   return (
-    <MainBox>
-    <div>
-      <OrderDiv>
-        <MyText><i class="fas fa-truck"></i>...Psst! Visste du att du får fri frakt om du handlar för över 500 SEK</MyText>
-      </OrderDiv>
-      <div>
-          <HeaderLine></HeaderLine>
-          <MyImage src={ '/img/dinavaror.jpg'} /></div> 
-          {cartItems.length <=0 
-      ? (<h2>Varukorgen är tom</h2>) 
-      : (cartItems.map((item, index) => (
-        <>
-          <MyContainer>
-            <MyRow>
-              <Col md={2}>
-                <Image src={item.imageurl} width="70px"></Image>
-              </Col>
-              <Col md={3}>
-               <Link to={`/product/${item.id}`}>{item.name} </Link>
-              </Col>
-
-              <Col md={3} >
-                <StyledCounter>
-                  <Dot onClick={()=> decrementQty(index) } btnType={'counterBtn'}>-</Dot>
-                  <StyledNumber>{item.qtyInCart}</StyledNumber>
-                  <Dot onClick={()=> increaseQty(index)} btnType={'counterBtn'}>+</Dot>
-                </StyledCounter>
-              </Col>
-              <Col md={2}>{item.price * item.qtyInCart} SEK</Col>
-              <Col md={2}><i onClick={() => {remove(index)}} class="fas fa-trash-alt"></i></Col>
-
-            </MyRow>
-          </MyContainer>
-          </> )) )} {!cartItems.length <=1 && 
-          <div>
-            <PriceDiv>
-              <Button onClick={() => {setAnimation(true)}}>ANGE VÄRDEKOD</Button>
-              <h5>Totalt i varukorgen: {totalPrice} SEK</h5>
-              </PriceDiv>
-              {!cartItems.length <= 0 && 
-                <TotalDiv> 
-                <h3>Fraktkostnad {totalPrice > 499 
-                ? 0
-                : shipping
-              } SEK</h3>
-                {discountValue
-                ? <><RedText>rabatt: {theDiscountValue} SEK </RedText>
-                  <h3>Att Betala: {withDiscount + shipping} SEK</h3></>
-                : <h3>Att betala: {totalPrice + shipping} SEK</h3>
-                }
-                </TotalDiv>
-              }
-
-            <HeaderLine></HeaderLine>
-            <MyImage src={ '/img/slutforkop.jpg'} /> </div> } </div>
-            <DiscountBox />
-  </MainBox> )
+<MainBox>
+	<div>
+		<OrderDiv>
+			<MyText><i class="fas fa-truck"></i>...Psst! Visste du att du får fri frakt om du handlar för över 500 SEK</MyText>
+		</OrderDiv>
+		<div>
+			<HeaderLine />
+			<MyImage src={ '/img/dinavaror.jpg'} />
+		</div> {cartItems.length
+		<=0 ? (<h2>Varukorgen är tom</h2>) : (cartItems.map((item, index) => (
+			<>
+				<MyContainer>
+					<MyRow>
+						<Col md={2}>
+							<Image src={item.imageurl} width="70px"></Image>
+						</Col>
+						<Col md={3}>
+							<Link to={`/product/${item.id}`}>{item.name} </Link>
+						</Col>
+						<Col md={3}>
+							<StyledCounter>
+								<Dot onClick={()=> decrementQty(index) } btnType={'counterBtn'}>-</Dot>
+								<StyledNumber>{item.qtyInCart}</StyledNumber>
+								<Dot onClick={()=> increaseQty(index)} btnType={'counterBtn'}>+</Dot>
+							</StyledCounter>
+						</Col>
+						<Col md={2}>{item.price * item.qtyInCart} SEK</Col>
+						<Col md={2}><i onClick={()=> {remove(index)}} class="fas fa-trash-alt"></i></Col>
+					</MyRow>
+				</MyContainer>
+				</> )) )} {!cartItems.length
+				<=1 && <div>
+					<PriceDiv>
+						<Button onClick={()=> {setAnimation(true)}}>ANGE VÄRDEKOD</Button>
+						<h5>Totalt i varukorgen: {totalPrice} SEK</h5> </PriceDiv> {!cartItems.length
+					<=0 && <TotalDiv>
+						<h3>Fraktkostnad {myShipping} SEK</h3> 
+            {discountValue 
+              ? <>
+							<RedText>rabatt: {theDiscountValue} SEK </RedText>
+							<h3>Att Betala: {withDiscount + myShipping} SEK</h3>
+							</> 
+              : <h3>Att betala: {totalPrice + myShipping} SEK</h3> } </TotalDiv> }
+							<HeaderLine />
+							<MyImage src={ '/img/slutforkop.jpg'} /> </div> } </div>
+	<DiscountBox />
+	<KlarnaDiv />
+</MainBox> )
 }
 
 export default CartScreen
